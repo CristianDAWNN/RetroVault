da5b4a8d3960295636ebadc8a3b2b32b406a6ada
Cristian <cristian_suarez_1992@hotmail.es>
2026-01-25 19:52:49 +0000

    Agregar configuración inicial de Maven y estructura del proyecto Spring Boot

---
+++ retrovault/retrovault/src/main/java/com/retrovault/retrovault/RetrovaultApplication.java
@@ -0,0 +1,13 @@
+package com.retrovault.retrovault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrovaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrovaultApplication.class, args);
+	}
+
+}
